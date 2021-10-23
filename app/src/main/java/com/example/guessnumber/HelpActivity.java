3e7f57624613ef601de5773dfc768bb15fa1b598
Alexander Strakhov <mrfaster98@gmail.com>
2021-10-23 13:44:27 +0300

    managing git files with .gitignore part 2

---
+++ app/src/main/java/com/example/guessnumber/HelpActivity.java
@@ -0,0 +1,13 @@
+package com.example.guessnumber;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
---
+++ app/src/main/java/com/example/guessnumber/NumberGenerator.java
@@ -0,0 +1,59 @@
+package com.example.guessnumber;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class NumberGenerator {
+
+    private List<Integer> GeneratePrimeNumbersListWithBruteForce(int n) {
+        List<Integer> primeNumbers = new LinkedList<>();
+        //Put every prime digit in the range between 2 and n in the list
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeBruteForce(i)) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
+
+    private Integer GenerateNonPrimeDigit(Integer digit_upper_boundary) {
+        Integer random_integer = new Random().nextInt(digit_upper_boundary - 2) + 2;
+
+        if (isPrimeBruteForce(random_integer))
+            random_integer= GenerateNonPrimeDigit(digit_upper_boundary);
+
+        return random_integer;
+    }
+
+    //Check if number is Prime
+    public boolean isPrimeBruteForce(Integer number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Return a list, filled with generated numbers
+    public List<Integer> GetRandomNumbers(int total_numbers_count, int digit_upper_boundary) {
+        List<Integer> Numbers = new LinkedList<>();
+        List<Integer> PrimeNumbers = GeneratePrimeNumbersListWithBruteForce(digit_upper_boundary);
+        //Get total count of prime numbers
+        int prime_count = new Random().nextInt(total_numbers_count - 1);
+        //Fill the list with random number of prime digits
+        for (int i = 0; i < prime_count + 1; i++){
+            int index_of_prime_number = new Random().nextInt(PrimeNumbers.size());
+            Integer prime_digit = PrimeNumbers.get(index_of_prime_number);
+            Numbers.add(prime_digit);
+        }
+        //Filling the list with non-prime digits
+        for (int i = 0; i < total_numbers_count - prime_count - 1; i++)
+            Numbers.add(GenerateNonPrimeDigit(digit_upper_boundary));
+        //Shuffle the list
+        Collections.shuffle(Numbers);
+        return Numbers;
+    }
+}
